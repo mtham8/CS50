d6eb8ecc8a92afb1ae704a285ca94b36b4c99bee
Michelle <mtham101@gmail.com>
2016-09-13 09:59:38 -0700

    covering arrays in c

--- ProgrammingInC/C1-Fundamentals.c
+++ ProgrammingInC/C1-Fundamentals.c
@@ -57,3 +57,55 @@ C errors to avoid: Forgetting to increment the counter inside the while loop -
 If you forget to increment the counter, you get an infinite loop (the loop never ends).
 */
 
+/*
+Arrays
+*/
+int a[3]; // the three seperate integers inside this array is accessed by an index
+a[0] = 12;
+a[1] = 9;
+a[2] = 14;
+
+#include <stdio.h>
+
+#define MAX 10 // the #define line declares a constant named MAX and sets it to 10
+// constant names are traditionally written in all caps
+
+int a; // declare an array of integers in C
+int rand_seed=10;
+
+/* from K&R
+   - returns random number between 0 and 32767.*/
+int rand()
+{
+    rand_seed = rand_seed * 1103515245 +12345;
+    return (unsigned int)(rand_seed / 65536) % 32768;
+}
+
+int main()
+{
+    int i,t,x,y;
+
+    /* fill array */
+    for (i=0; i < MAX; i++)
+    {
+        a=rand();
+        printf("%d\n",a);
+    }
+
+    /* bubble sort the array*/
+    for (x=0; x < MAX-1; X++)
+      for(y=0; y < MAX-1; y++)
+        if (a > a[y+1])
+        {
+          t=a;
+          a=a[y+1];
+          a[y+1]=t;
+        }
+
+    /* print the sorted array */
+    printf('-----\n');
+    for(i=0; i < MAX; i++)
+      printf('%d\n', a);
+}
+
+
