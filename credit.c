943e821c2c97c1950c50f4a7372a6f5399f87aad
Michelle Tham <mtham8@users.noreply.github.com>
2016-09-16 17:53:58 -0700

    start pset1.2

---
+++ credit.c
@@ -0,0 +1,6 @@
+/*
+Description:
+In credit.c, write a program that prompts the user for a credit card number and then reports (via printf) whether
+it is a valid American Express, MasterCard, or Visa card number, per the definitions of each’s format herein.
+*/
+
